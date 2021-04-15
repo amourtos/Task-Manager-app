3deb89f2e823d5d18374b973e63af69746b67a54
Alex Mourtos <amourtos@gmail.com>
2021-04-15 14:24:56 -0400

    delete todo is working

--- src/todoItem/todoItem.js
+++ src/todoItem/todoItem.js
@@ -1,59 +1,58 @@
 import React, { useEffect, useState } from "react";
-import { Card, Menu, Button, Tooltip } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
-import { useStore, GET_TODOLIST, TOGGLE_COMPLETE, GET_TODO } from "../store/store";
+import { Card, Button } from "antd";
+import { useStore } from "../store/store";
 import { deleteTodo, toggleComplete, getMyTodos, getTodo } from "../fetch/fetch";
-import ActionButton from "antd/lib/modal/ActionButton";
 
 function TodoItem(props) {
   const user = useStore((state) => state.user);
   const [todo, setTodo] = useState({});
-  // const dispatch = useStore((state) => state.dispatch);
-  // const store = useStore();
 
   const toggleCompleted = async () => {
     await toggleComplete(user.token, props._id);
     const newTodo = await getTodo(user.token, props._id);
     setTodo(newTodo);
     console.log(todo);
-    props.updateMatter(Math.random());
+    // props.updateMatter(Math.random());
   };
 
   const deleteTodoFunction = async () => {
-    let newTodo = await deleteTodo(user.token, props._id);
-    // const newTodo = await getTodo(user.token, props._id);
+    await deleteTodo(user.token, props._id);
+    const newTodo = await getTodo(user.token, props._id);
     setTodo(newTodo);
-    console.log(newTodo);
+    await getMyTodos(user.token).then((data) => console.log(data));
     props.updateMatter(Math.random());
   };
 
   useEffect(() => {
     const someBullShit = async () => {
       const updatedTodo = await getTodo(user.token, props._id).catch((err) => console.log(err));
-      console.log(updatedTodo);
       setTodo(updatedTodo);
     };
     someBullShit();
-  }, []);
+  }, [user]);
 
   return (
     <div className="todoItem">
-      <li>
-        <Card title={<h3>{todo.title}</h3>} style={{ width: 300 }}>
-          <p>details: {todo.details}</p>
-          <p>createdAt: {todo.createdAt}</p>
-          <p>dueDate: {todo.dueDate}</p>
-          <p>completed: {todo.completed === true ? "completed" : "Not Completed"}</p>
+      {todo === null ? (
+        <div></div>
+      ) : (
+        <li>
+          <Card title={<h3>{todo.title}</h3>} style={{ width: 300 }}>
+            <p>details: {todo.details}</p>
+            <p>createdAt: {todo.createdAt}</p>
+            <p>dueDate: {todo.dueDate}</p>
+            <p>completed: {todo.completed === true ? "completed" : "Not Completed"}</p>
 
-          <Button type="primary" shape="circle" onClick={toggleCompleted}>
-            Toggle Complete
-          </Button>
+            <Button type="primary" shape="circle" onClick={toggleCompleted}>
+              Toggle Complete
+            </Button>
 
-          <Button type="primary" shape="circle" onClick={deleteTodoFunction}>
-            Delete
-          </Button>
-        </Card>
-      </li>
+            <Button type="primary" shape="circle" onClick={deleteTodoFunction}>
+              Delete
+            </Button>
+          </Card>
+        </li>
+      )}
     </div>
   );
 }
--- src/todoList/todoList.js
+++ src/todoList/todoList.js
@@ -1,16 +1,12 @@
-import React, { useEffect, useImperativeHandle, useState } from "react";
-import { getMyTodos, toggleComplete } from "../fetch/fetch";
-import { GET_TODOLIST, TOGGLE_COMPLETE, useStore } from "../store/store";
+import React, { useEffect, useState } from "react";
+import { getMyTodos } from "../fetch/fetch";
+import { useStore } from "../store/store";
 import TodoItem from "../todoItem/todoItem";
 
 function TodoList(props) {
   const user = useStore((state) => state.user);
-  // const dispatch = useStore((state) => state.dispatch);
-  // const store = useStore();
   const [dontMatter, setDontMatter] = useState(0);
   const [todoList, setTodoList] = useState([]);
-  // const token = window.localStorage.user.slice(10, -2);
-  // console.log(window.localStorage.user.slice(10, -2));
   useEffect(() => {
     const something = async () => {
       const todos = await getMyTodos(user.token)
@@ -24,20 +20,19 @@ function TodoList(props) {
     <section className="main">
       <h3>Todo List</h3>
       <ul className="todo-list">
-        {todoList &&
-          todoList.map((props) => (
-            <TodoItem
-              updateMatter={setDontMatter}
-              title={props.title}
-              details={props.details}
-              completed={props.completed}
-              dueDate={props.dueDate}
-              createdAt={props.createdAt}
-              category={props.category}
-              _id={props._id}
-            />
-          ))}
-        <TodoItem />
+        {todoList.map((props) => (
+          <TodoItem
+            key={props._id}
+            updateMatter={setDontMatter}
+            title={props.title}
+            details={props.details}
+            completed={props.completed}
+            dueDate={props.dueDate}
+            createdAt={props.createdAt}
+            category={props.category}
+            _id={props._id}
+          />
+        ))}
       </ul>
     </section>
   );
